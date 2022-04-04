f01c2e8b548eebc4f9fa273d9d01304bcd53e8d0
sumin <l08c0739@gmail.com>
2022-04-04 21:26:40 +0900

    feat: gtag 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,32 @@
 import { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../styles/global.scss';
 import ContentLayout from '../components/ContentLayout';
 import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
 import MainLayout from '../components/MainLayout';
+import * as gtag from '../lib/gtag';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const { pathname } = useRouter();
   const isArticleView = pathname.includes('/article');
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace(router.asPath);
+  });
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <>
