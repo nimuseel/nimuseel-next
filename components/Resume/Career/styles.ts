2126b9c18fc96f4c9de4b5ae5e967011ac686f04
nimuseel <l08c0739@gmail.com>
2022-08-18 23:38:34 +0900

    refactor: Skills 컴포넌트 분리

--- components/Resume/Career/styles.ts
+++ components/Resume/Career/styles.ts
@@ -15,22 +15,6 @@ export const CompanyAndProjectPeriod = css`
   }
 `;
 
-const ProjectSummary = styled.p`
-  margin: 0;
-  font-size: 17px;
-  color: #000;
-  font-weight: bold;
-`;
-
-const Skills = styled.p`
-  margin: 0;
-  font-size: 17px;
-  color: #000;
-  font-weight: bold;
-`;
-
 export default {
   CareerSection,
-  ProjectSummary,
-  Skills,
 };
